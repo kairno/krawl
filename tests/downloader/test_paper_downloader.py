9839c2973ef5f7543661b348e93a36e96ac28d81
Fabrice Harel-Canada <fabrice@kairno.com>
2025-05-20 16:45:25 -0700

    adding parser base (not fully working yet)

--- tests/downloader/test_paper_downloader.py
+++ tests/downloader/test_paper_downloader.py
@@ -1,16 +1,16 @@
 import os
-import tempfile
 from krawl.downloader.paper_downloader import PaperDownloader
 
 def test_download_pdf():
     url = "https://arxiv.org/pdf/2106.14834.pdf"
-    with tempfile.TemporaryDirectory(prefix="paper_downloader_test_") as test_dir:
-        target_path = os.path.join(test_dir, "arxiv_2106.14834.pdf")
-        downloader = PaperDownloader()
-        success = downloader.download_pdf(url, target_path)
-        assert success
-        assert os.path.exists(target_path)
-        assert os.path.getsize(target_path) > 0
+    test_dir = os.path.join(os.path.dirname(__file__), "../test_data/")
+    os.makedirs(test_dir, exist_ok=True)
+    target_path = os.path.join(test_dir, "arxiv_2106.14834.pdf")
+    downloader = PaperDownloader()
+    success = downloader.download_pdf(url, target_path)
+    assert success
+    assert os.path.exists(target_path)
+    assert os.path.getsize(target_path) > 0
 
 def test_download_pdfs():
     test_urls = [
@@ -19,11 +19,12 @@ def test_download_pdfs():
         ("https://aclanthology.org/2022.acl-long.3.pdf", "2022.acl-long.3.pdf"),
         ("https://aclanthology.org/2022.acl-long.4.pdf", "2022.acl-long.4.pdf"),
     ]
-    with tempfile.TemporaryDirectory(prefix="paper_downloader_test_") as test_dir:
-        url_path_list = [(url, os.path.join(test_dir, fname)) for url, fname in test_urls]
-        downloader = PaperDownloader()
-        results = downloader.download_pdfs(url_path_list, wait_time=0.1)
-        for (url, path, success) in results:
-            assert success
-            assert os.path.exists(path)
-            assert os.path.getsize(path) > 0 
+    test_dir = os.path.join(os.path.dirname(__file__), "../test_data/")
+    os.makedirs(test_dir, exist_ok=True)
+    url_path_list = [(url, os.path.join(test_dir, fname)) for url, fname in test_urls]
+    downloader = PaperDownloader()
+    results = downloader.download_pdfs(url_path_list, wait_time=0.1)
+    for (url, path, success) in results:
+        assert success
+        assert os.path.exists(path)
+        assert os.path.getsize(path) > 0 
